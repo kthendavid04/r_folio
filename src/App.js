563d56187bfad7f7ad580c5e64db815f845c8c63
Kthendavid04 <kthendavid04@gmail.com>
2021-08-09 21:17:00 -0500

    setting up repo for gh-pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'semantic-ui-less/semantic.less'
+import './App.css';
+import HomepageLayout from './Components/HomepageLayout.js'
+
+
+function App() {
+  return (
+    <HomepageLayout />
+  );
+}
+
+export default App;
